001512e462dd65e6020e02478df4849f66dd6503
Samuel Torres <rilla@MacBook-Pro.local>
2025-09-25 17:21:57 -0400

    Finish Ex 3

--- newproverb.cpp
+++ newproverb.cpp
@@ -1,66 +1,33 @@
 // This program will allow the user to input from the keyboard
-// whether the last word to the following proverb should be party or country:
+// the word that should finish the following proverb:
 // "Now is the time for all good men to come to the aid of their ___"
-// Inputting a 1 will use the word party. Any other number will use the word country.
-
+//
 // SAMUEL TORRES
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-// Fill in the prototype of the function writeProverb.
-
-void writeProverb(int number);
+// Prototype: writeProverb now accepts a string ending to finish the proverb.
+void writeProverb(const string &ending);
 
 int main()
 {
-	int wordCode;
-
 	cout << "Given the phrase:" << endl;
-	cout << "Now is the time for all good men to come to the aid of their ___"
-		 << endl;
-	cout << "Input a 1 if you want the sentence to be finished with party" << endl;
-	cout << "Input a 2 if you want the sentence to be finished with country" << endl;
-	cout << "Please input your choice now" << endl;
-	cin >> wordCode;
-	while (wordCode != 1 && wordCode != 2)
-	{
-		cout << "Invalid choice. Enter 1 for 'party' or 2 for 'country': " << endl;
-		cin >> wordCode;
-	}
+	cout << "Now is the time for all good men to come to the aid of their ___" << endl;
+
+	cout << "Please input the word you would like to have finish the proverb" << endl;
+	string ending;
+	cin >> ending; // read a single word to finish the proverb
 	cout << endl;
 
-	writeProverb(wordCode);
+	writeProverb(ending);
 
 	return 0;
 }
 
-//	******************************************************************************
-//	writeProverb
-//
-//	task:	  This function prints a proverb. The function takes a number
-//	          from the call. If that number is a 1 it prints "Now is the time
-//	          for all good men to come to the aid of their party."
-//	          Otherwise, it prints "Now is the time for all good men
-//	          to come to the aid of their country."
-//	data in:  code for ending word of proverb (integer)
-//	data out: no actual parameter altered
-//
-//	*****************************************************************************
-
-void writeProverb(int number)
+// writeProverb: prints the proverb using the provided ending word
+void writeProverb(const string &ending)
 {
-	if (number == 1)
-	{
-		cout << "Now is the time for all good men to come to the aid of their party." << endl;
-	}
-	else if (number == 2)
-	{
-		cout << "Now is the time for all good men to come to the aid of their country." << endl;
-	}
-	else
-	{
-		cout << "Invalid code passed to writeProverb()." << endl;
-	}
+	cout << "Now is the time for all good men to come to the aid of their " << ending << "." << endl;
 }
